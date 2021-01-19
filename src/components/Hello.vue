daded5e6138dd553837b2ff0d9343af863d0f7f3
WattanachaiBoom <boom_sirisuk@hotmail.com>
2021-01-19 23:07:21 +0700

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <div class="home">
+      <h2 class="cyan accent-3 red--text text--darken-3">Wattanachai</h2>
+      <h2 class="display-4 font-weight-black font-italic">Vuetify tutorial</h2>
+      <v-btn class="pink" dark small>
+        <v-icon> mdi-thumb-up</v-icon>
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
